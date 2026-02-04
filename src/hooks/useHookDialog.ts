2c91594894804417cb95e78703d6a62784cf0773
rokku-x <rokku-x@users.noreply.github.com>
2026-02-04 13:29:11 +0800

    feat: expose dialog instance context and control functions in requestDialog return type

--- src/hooks/useHookDialog.ts
+++ src/hooks/useHookDialog.ts
@@ -1,8 +1,6 @@
-import React, { useCallback } from "react";
 import { useBaseModal } from '@rokku-x/react-hook-modal';
-import ModalWindow from "@/components/ModalWindow";
 import { useDialogStore } from "@/store/dialogStore";
-import type { FormDataObject, ConfirmConfig, ConfirmInstance, UseHookDialogConfig, ValidValue, ModalAction } from '@/types'
+import type { FormDataObject, ConfirmConfig, ConfirmInstance, UseHookDialogConfig, ValidValue, ModalAction, RequestDialogReturnType, UseHookDialogReturnType } from '@/types'
 
 /**
  * Hook for displaying confirmation dialogs and alerts.
@@ -40,58 +38,45 @@ import type { FormDataObject, ConfirmConfig, ConfirmInstance, UseHookDialogConfi
  * ```
  */
 export default function useHookDialog<T = ValidValue>(defaultConfig?: UseHookDialogConfig) {
-    const { instances, addInstance, removeInstance, getInstance } = useDialogStore();
+    const { addInstance, handleAction, handleClose, getContext } = useDialogStore();
     const baseModalInstance = useBaseModal();
-
-    const handleClose = useCallback((id: string) => {
-        const modal = getInstance(id);
-        if (!modal) return;
-        baseModalInstance.popModal(id);
-
-        if (modal.config.rejectOnCancel !== false) modal.reject(modal.config.defaultCancelValue);
-        else modal.resolve(modal.config.defaultCancelValue);
-        removeInstance(id);
-    }, [getInstance, removeInstance, baseModalInstance]);
-
-    const handleAction = useCallback((id: string, action: ModalAction) => {
-        const modal = getInstance(id);
-        if (!modal) return;
-        baseModalInstance.popModal(id);
-
-        if (action.isCancel) {
-            if (modal.config.rejectOnCancel !== false) modal.reject(action.value);
-            else modal.resolve(action.value);
-        } else {
-            modal.resolve(action.value);
-        }
-
-        removeInstance(id);
-    }, [getInstance, removeInstance, baseModalInstance]);
 
     // Overloads allow consumers to narrow the Promise result type:
     // - When `isReturnSubmit: true` is provided in the config, the default result is a `FormDataObject` (but can be overridden via generic)
     // - Otherwise the default is `T` (the hook's generic)
     function requestDialog<U = FormDataObject>(config: ConfirmConfig & { isReturnSubmit: true }): Promise<U>
-    function requestDialog<U = T>(config: ConfirmConfig): Promise<U>
-    function requestDialog<U = any>(config: ConfirmConfig): Promise<U> {
-        return new Promise<U>((resolve, reject) => {
-            const id = Math.random().toString(36).substring(2, 6);
-            const mergedConfig: ConfirmConfig = {
-                ...defaultConfig,
-                ...config,
-                classNames: {
-                    ...defaultConfig?.classNames,
-                    ...config.classNames,
-                },
-                styles: {
-                    ...defaultConfig?.styles,
-                    ...config.styles,
-                },
-                variantStyles: {
-                    ...defaultConfig?.variantStyles,
-                    ...config.variantStyles,
-                },
-            };
+    function requestDialog<U = T>(config: ConfirmConfig): Promise<U> {
+        const id = Math.random().toString(36).substring(2, 6);
+
+        //check actions if there are multiple focused actions
+        const focusedActions = config.actions?.flat().filter(action => action.isFocused) || [];
+        if (focusedActions.length > 1) console.warn(`useHookDialog: Multiple actions are marked as isFocused in dialog "${id}". Only one action should be focused.`);
+
+        //check actions if there is at least one action marked as cancel
+        const hasCancelAction = config.actions?.some(row => row.some(action => action.isCancel));
+
+        // If no cancel action is defined, and backdropCancel config is undefined, we treat backdrop clicks as cancel
+        const backdropCancel = config.backdropCancel ?? defaultConfig?.backdropCancel ?? !hasCancelAction;
+
+        const mergedConfig: ConfirmConfig = {
+            ...defaultConfig,
+            ...config,
+            classNames: {
+                ...defaultConfig?.classNames,
+                ...config.classNames,
+            },
+            styles: {
+                ...defaultConfig?.styles,
+                ...config.styles,
+            },
+            variantStyles: {
+                ...defaultConfig?.variantStyles,
+                ...config.variantStyles,
+            },
+            backdropCancel,
+        };
+
+        const promise = new Promise<U>((resolve, reject) => {
             const newInstance: ConfirmInstance<U> = {
                 id,
                 config: mergedConfig,
@@ -99,10 +84,16 @@ export default function useHookDialog<T = ValidValue>(defaultConfig?: UseHookDia
                 reject,
             };
             addInstance(newInstance);
-
-            newInstance.id = baseModalInstance.pushModal(id, React.createElement(ModalWindow, { config: mergedConfig, modalWindowId: id, handleClose, handleAction }));
         });
+
+        Object.defineProperties(promise, {
+            'context': { get() { return getContext(id); }, enumerable: true, configurable: false },
+            'id': { get() { return id }, enumerable: true, configurable: false }
+        })
+
+        return promise as RequestDialogReturnType<U>;
     };
 
-    return [requestDialog];
+    return [requestDialog, getContext] as UseHookDialogReturnType<T>;
 }
+
--- src/store/dialogStore.ts
+++ src/store/dialogStore.ts
@@ -1,4 +1,7 @@
-import type { ConfirmInstance, ValidValue } from '@/types';
+import ModalWindow from '@/components/ModalWindow';
+import type { ConfirmInstance, ModalAction, ValidValue } from '@/types';
+import { useBaseModal } from '@rokku-x/react-hook-modal';
+import React from 'react';
 import { create } from 'zustand';
 
 /**
@@ -16,9 +19,21 @@ interface DialogStore<T = ValidValue> {
     /** Remove a dialog instance from the store by ID */
     removeInstance: (id: string) => void;
 
+    /** Handle closing a dialog instance */
+    handleClose: (id: string, isForceCancel?: boolean) => void;
+
+    /** Handle an action taken on a dialog instance */
+    handleAction: (id: string, action: ModalAction) => void;
+
     /** Get a dialog instance by ID */
     getInstance: (id: string) => ConfirmInstance<T> | undefined;
+
+    /** Get the context of a dialog instance by ID */
+    getContext: (id: string) => { id: string; config: any; forceCancel: () => void } | undefined;
 }
+
+// Get base modal instance for managing modal stack
+const baseModalInstance = useBaseModal();
 
 /**
  * Zustand store for managing dialog instances.
@@ -32,13 +47,54 @@ interface DialogStore<T = ValidValue> {
  */
 export const useDialogStore = create<DialogStore<any>>((set, get) => ({
     instances: [],
-    addInstance: (instance: ConfirmInstance) =>
+    addInstance: (instance: ConfirmInstance) => {
         set((state) => ({
             instances: [...state.instances, instance],
-        })),
+        }));
+        instance.id = baseModalInstance.pushModal(instance.id, React.createElement(ModalWindow, { config: instance.config, modalWindowId: instance.id, handleClose: get().handleClose, handleAction: get().handleAction }));
+    },
     removeInstance: (id: string) =>
         set((state) => ({
             instances: state.instances.filter((m) => m.id !== id),
         })),
+    handleClose: (id: string, isForceCancel: boolean = false) => {
+        const modal = get().getInstance(id);
+        if (!modal) return;
+        baseModalInstance.popModal(id);
+
+        if (modal.config.rejectOnCancel !== false || isForceCancel) modal.reject(modal.config.defaultCancelValue);
+        else modal.resolve(modal.config.defaultCancelValue);
+
+        get().removeInstance(id);
+    },
+    handleAction: (id: string, action: ModalAction) => {
+        const modal = get().getInstance(id);
+        if (!modal) return;
+        baseModalInstance.popModal(id);
+
+        if (action.isCancel) {
+            if (modal.config.rejectOnCancel !== false) modal.reject(action.value);
+            else modal.resolve(action.value);
+        } else {
+            modal.resolve(action.value);
+        }
+
+        get().removeInstance(id);
+    },
+    getContext: (id: string) => {
+        const modal = get().getInstance(id);
+        if (!modal) throw new Error(`Dialog instance with id "${id}" not found.`);
+        return {
+            id: modal.id,
+            config: modal.config,
+            forceCancel: (forceReject: boolean = true) => get().handleClose(id, forceReject),
+            forceAction: (action: ModalAction) => { get().handleAction(id, action); },
+            forceDefault: () => {
+                const firstDefaultAction = modal.config.actions?.flat().find((action) => action.isFocused);
+                if (!firstDefaultAction) throw new Error(`No default action (isFocused) defined for dialog instance with id "${id}".`);
+                get().handleAction(id, firstDefaultAction);
+            }
+        };
+    },
     getInstance: (id: string) => get().instances.find((m) => m.id === id) as ConfirmInstance | undefined,
 }));
